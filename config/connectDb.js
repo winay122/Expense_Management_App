2cb99ab19dc0bbc1cfbf50cc23fd86ded471771e
Vinay Kumar <vinaykumary122@gmail.com>
2024-02-08 13:28:31 +0530

    final deployment
    expanse APP

---
+++ config/connectDb.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {}).then((data) => {
+      console.log(`Connected to MongoDB: ${data.connection.host}`);
+    });
+    console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
+  } catch (error) {
+    console.log(`${error}`.bgRed);
+  }
+};
+
+module.exports = connectDb;
